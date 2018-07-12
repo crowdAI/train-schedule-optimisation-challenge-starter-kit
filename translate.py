8b1341dfc4f362a2e586b72e98ba58e47daa6249
Jordi Julian (IT-SCI-TPR-PMP) <julian.jordi@sbb.ch>
2018-07-12 09:13:42 +0200

    initial commit

---
+++ translate.py
@@ -0,0 +1,107 @@
+import json
+import csv
+import collections.abc
+import pathlib
+
+
+# build the translation dicts
+############################################################################################################
+GER_2_ENG = dict()
+ENG_2_GER = dict()
+with open("translation_table.csv") as fp:
+    a = csv.reader(fp, skipinitialspace=True)
+    next(a)  # skip first line
+    for ger, eng in a:
+        GER_2_ENG[ger] = eng
+        ENG_2_GER[eng] = ger
+############################################################################################################
+
+# main function that does the translation recursively
+############################################################################################################
+def translate_scenario_file(d, translation_function):
+    """
+    Convert a nested dictionary from one convention to another.
+    Args:
+        d (dict): SBB scenario file to be converted.
+        translation_function (func): function that translates a key into another language
+    Returns:
+        Dictionary with the new keys.
+    """
+    new = dict()
+    for k, v in d.items():
+        new_v = v
+        if k == "parameters":  # do not translate parameters
+            # print("i'm in parameters, skip them")
+            new[k] = v
+            continue
+
+        if isinstance(v, collections.abc.Mapping):
+            # print(f"treating a {k}, it has a dict as value")
+            # print(v)
+            new_v = translate_scenario_file(v, translation_function)
+
+        elif isinstance(v, collections.abc.MutableSequence) and len(v) > 0 and isinstance(v[0], dict):  # we have some "trivial" lists, like abschnittskennzeichen = ["C"]. Don't call recursion for these
+            # print(f"treating {k}, it has a list as value")
+            # print(v)
+            new_v = list()
+            for x in v:
+                new_v.append(translate_scenario_file(x, translation_function))
+        new[translation_function(k)] = new_v
+    return new
+############################################################################################################
+
+# translation functions
+############################################################################################################
+def translate_to_eng(key):
+    #print(f"translating {key}")
+    try:
+        return GER_2_ENG[key]
+    except KeyError:
+        print(f"WARNING: don't know how to translate '{key}''. I will leave it as is.")
+        return key
+        # raise Exception(f"cant' translate {key}")
+
+def translate_to_ger(key):
+    #print(f"translating {key}")
+    try:
+        return ENG_2_GER[key]
+    except KeyError:
+        print(f"WARNING: don't know how to translate {key}. I will leave it as is.")
+        return key
+        # raise Exception(f"cant' translate {key}")
+############################################################################################################
+
+# convert a single scenario
+############################################################################################################
+file = pathlib.Path("sample_scenario.json")
+
+with open(file) as fp:
+    to_translate = json.load(fp)
+
+translated = translate_scenario_file(to_translate, translate_to_eng)
+
+# dump the json if desired
+out_suffix = "_eng"  # e.g. filename.json will be translated into filename_eng.json
+out_file = file.parent / (str(file.stem) + out_suffix + str(file.suffix))
+
+with out_file.open("w") as fp:
+    json.dump(translated, fp)
+############################################################################################################
+
+# convert all json-files in a directory.
+############################################################################################################
+directory = pathlib.Path.cwd()/"samples"/"problem_instances"
+out_directory = directory  # write translated scenarios into this directory
+out_suffix = "_eng"  # e.g. filename.json will be translated into filename_eng.json
+g = directory.glob('*.json')
+
+for file in g:
+    print("translating " + file.name)
+    with file.open() as fp:
+        to_translate = json.load(fp)
+    translated = translate_scenario_file(to_translate, translate_to_eng)
+
+    out_file = directory / (str(file.stem) + out_suffix + str(file.suffix))
+    with out_file.open("w") as fp:
+        json.dump(translated, fp)
+############################################################################################################
---
+++ validate_solution.py
@@ -0,0 +1,36 @@
+import requests
+
+AUTH = ('ebrown', 'plutonium94')
+SCENARIO_UPLOAD_ENDPOINT = "https://fluxer.app.sbb.ch/backend/verkehrsplan/uploadVerkehrsplan"
+SOLUTION_VALIDATION_ENDPOINT = "https://fluxer.app.sbb.ch/backend/loesung-validator/validateFile"
+
+scenario = "samples/sample_scenario_simple.json"
+solution = "samples/sample_scenario_simple_solution.json"
+
+
+def do_loesung_validation(scenario, solution):
+
+    # upload scenario first (may not be available in the store)
+    scenario_file = {"verkehrsplan": open(scenario, 'rb')}
+    upload_response = requests.post(SCENARIO_UPLOAD_ENDPOINT, files=scenario_file, auth=AUTH)
+    print(f"upload finished with status {upload_response}")
+
+    # now we can validate
+    solution_file = {"loesung": open(solution, 'rb')}
+    validation_response = requests.post(SOLUTION_VALIDATION_ENDPOINT, files=solution_file, auth=AUTH)
+    print(f"validation finished with status {validation_response}")
+    return validation_response.json()
+
+validation_result = do_loesung_validation(scenario, solution)
+
+warnings = [x for x in validation_result["regelVerletzungen"] if x["severity"] == "warning"]
+errors = [x for x in validation_result["regelVerletzungen"] if x["severity"] == "error"]
+
+if len(errors) > 0:
+    print(f"the solution has {len(errors)} errors. It will not be accepted as a feasible solution. "
+          f"See the error messages for details.")
+
+elif len(warnings) > 0:
+    print(f"the solution has {len(warnings)} warnings. It will be accepted as a feasible solution. However, it will "
+          f"incur {validation_result['objectiveValue']} penalty points in the grader.")
+

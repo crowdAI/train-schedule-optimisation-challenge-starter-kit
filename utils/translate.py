b5b0d6e8ee2c79101c4c44eb127c0e5d45e16c98
Jordi Julian (IT-SCI-TPR-PMP) <julian.jordi@sbb.ch>
2018-07-30 16:55:30 +0200

    script documented and executable

--- utils/translate.py
+++ utils/translate.py
@@ -1,3 +1,10 @@
+"""
+Helper script to translate a problem_instance or a solution from German into English or the other way around.
+
+Usage:
+- Execute as script to translate files, or a whole directory, and get back translated files. Type 'python translate.py -h' for usage instructions.
+- For interactive use in a Python session, import the module and use the 'translate_scenario_file' function to translate deserialized JSON-instances.
+"""
 import json
 import csv
 import collections.abc
@@ -22,29 +29,26 @@ with open("translation_table.csv") as fp:
 ############################################################################################################
 def translate_scenario_file(d, translation_function):
     """
-    Convert a nested dictionary from one language to another.
+    Convert a problem_instance or a solution from German into English, or vice-versa
+
+
     Args:
-        d (dict): SBB scenario file to be converted.
-        translation_function (func): function that translates a key into another language
+        d (dict): The deserialized JSON of a problem_instance or a solution. E.g. as obtained from json.load(path-to-problem-instance)
+        translation_function (func): function that translates a key into another language. Either 'translate_to_eng' or 'translate_to_ger'
     Returns:
-        Dictionary with the new keys.
+        Dictionary with the translated keys.
     """
     new = dict()
     for k, v in d.items():
         new_v = v
         if k == "parameters":  # do not translate parameters
-            # print("i'm in parameters, skip them")
             new[k] = v
             continue
 
         if isinstance(v, collections.abc.Mapping):
-            # print(f"treating a {k}, it has a dict as value")
-            # print(v)
             new_v = translate_scenario_file(v, translation_function)
 
         elif isinstance(v, collections.abc.MutableSequence) and len(v) > 0 and isinstance(v[0], dict):  # we have some "trivial" lists, like abschnittskennzeichen = ["C"]. Don't call recursion for these
-            # print(f"treating {k}, it has a list as value")
-            # print(v)
             new_v = list()
             for x in v:
                 new_v.append(translate_scenario_file(x, translation_function))
@@ -61,7 +65,6 @@ def translate_to_eng(key):
     except KeyError:
         print(f"WARNING: don't know how to translate '{key}''. I will leave it as is.")
         return key
-        # raise Exception(f"cant' translate {key}")
 
 def translate_to_ger(key):
     #print(f"translating {key}")
@@ -70,66 +73,50 @@ def translate_to_ger(key):
     except KeyError:
         print(f"WARNING: don't know how to translate {key}. I will leave it as is.")
         return key
-        # raise Exception(f"cant' translate {key}")
 ############################################################################################################
 
-# convert a single scenario
-############################################################################################################
-# file = pathlib.Path("../sample_files/sample_scenario.json")
-#
-# with open(file) as fp:
-#     to_translate = json.load(fp)
-#
-# translated = translate_scenario_file(to_translate, translate_to_eng)
-#
-# # dump the json if desired
-# out_suffix = "_eng"  # e.g. filename.json will be translated into filename_eng.json
-# out_file = file.parent / (str(file.stem) + out_suffix + str(file.suffix))
-#
-# with out_file.open("w") as fp:
-#     json.dump(translated, fp)
-############################################################################################################
+# Helper function
+def write_json(d, path, suffix=""):
+    out_file = path.parent / (str(path.stem) + out_suffix + str(path.suffix))
+    with out_file.open("w") as fp:
+        json.dump(d, fp, indent="\t")
+    print(f"Wrote file {out_file}")
 
-# convert all json-files in a directory.
-############################################################################################################
-# directory = pathlib.Path.cwd()/"samples"/"problem_instances"
-# out_directory = directory  # write translated scenarios into this directory
-# out_suffix = "_eng"  # e.g. filename.json will be translated into filename_eng.json
-# g = directory.glob('*.json')
-#
-# for file in g:
-#     print("translating " + file.name)
-#     with file.open() as fp:
-#         to_translate = json.load(fp)
-#     translated = translate_scenario_file(to_translate, translate_to_eng)
-#
-#     out_file = directory / (str(file.stem) + out_suffix + str(file.suffix))
-#     with out_file.open("w") as fp:
-#         json.dump(translated, fp)
-############################################################################################################
 
 if __name__ == "__main__":
-    print("entering main")
     parser = argparse.ArgumentParser(prog='translate.py')
-    parser.add_argument("file", help="path to the file to be translated. Must be a JSON in problem instance or solution data format")
+    parser.add_argument("path", help="path to directory (or individual file) to be translated. If a directory, each path in it will be translated (non-recursively). For this to work each path must be a JSON-path conforming to either the challenge input or output data model")
     parser.add_argument("from_into",
                         choices=["GER->ENG", "ENG->GER"],
-                        help="tuple (from-language -> into-language) of the languages to be translated. Allowed are GER->ENG for translating German into English and vice-versa ENG->GER")
+                        help="tuple (from-language -> into-language) of the languages. Allowed are GER->ENG for translating German into English and vice-versa ENG->GER")
     args = parser.parse_args()
 
     try:
-        with open(args.file) as fp:
-            d = json.load(fp)
+        path = pathlib.Path(args.path)
     except Exception as e:
-        print(f"Unable to deserialize {args.file}. Is it a valid JSON?")
+        print(f"Error: Can't create path for {args.path}. \n {e}")
         sys.exit(1)
+    else:  # determine all files to be translated
+        if path.is_dir():
+            to_translate = path.glob('*.json')
+        elif path.is_file():
+            to_translate = [path]  # make it iterable by turning it into a one-element-list
 
-    if args.from_into == "GER->ENG":
-        print("translating German to English")
-        translated = translate_scenario_file(d, translate_to_eng)
+        # translate each file
+        for s in to_translate:
+            try:
+                with open(s) as fp:
+                    d = json.load(fp)
+            except Exception as e:
+                print(f"Unable to deserialize {args.path}. Is it a valid JSON?")
+                sys.exit(1)
+            else:  # everything's fine, can translate
+                print(f"Translating {s}: {args.from_into}...")
+                if args.from_into == "GER->ENG":
+                    out_suffix = "_eng"
+                    translated = translate_scenario_file(d, translate_to_eng)
+                else:  # translate to German
+                    out_suffix = "_ger"
+                    translated = translate_scenario_file(d, translate_to_ger)
 
-    else:
-        print("Translateing English to German")
-        translated = translate_scenario_file(args.file, translate_to_ger)
-
-    # ToDo: Dump it out
+                write_json(translated, path=s, suffix=out_suffix)
